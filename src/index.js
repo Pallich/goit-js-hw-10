f8b5a0fae1b73bfedb28bfe8d0c3867de51df517
Diachenko Oleksandr <palllich@gmail.com>
2022-10-08 17:20:59 +0300

    create func fatch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', debounce(getData, DEBOUNCE_DELAY));
+
+function getData(e) {
+  const inputValue = e.target.value.trim();
+  fetchCountries(inputValue).then(data => console.log(data));
+}
